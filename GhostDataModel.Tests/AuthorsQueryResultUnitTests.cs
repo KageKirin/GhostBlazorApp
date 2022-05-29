6092970927b450e0ecc97bf4483f0278f89f109a
Chris Helmich <kagekirin@gmail.com>
2022-05-30 00:30:24 +0900

    feature: add unit test for Authors query result data

---
+++ GhostDataModel.Tests/AuthorsQueryResultUnitTests.cs
@@ -0,0 +1,71 @@
+using GhostDataModel;
+
+using System;
+using System.Diagnostics;
+using System.Text.Json;
+using NUnit.Framework;
+
+namespace GhostDataModel.Tests;
+
+public class AuthorsQueryResultUnitTests
+{
+    string testJson =
+        @"
+{
+    ""authors"": [
+        {
+            ""slug"": ""cameron"",
+            ""id"": ""5ddc9b9510d8970038255d02"",
+            ""name"": ""Cameron Almeida"",
+            ""profile_image"": ""https://docs.ghost.io/content/images/2019/03/1c2f492a-a5d0-4d2d-b350-cdcdebc7e413.jpg"",
+            ""cover_image"": null,
+            ""bio"": ""Editor at large."",
+            ""website"": ""https://example.com"",
+            ""location"": ""Cape Town"",
+            ""facebook"": ""example"",
+            ""twitter"": ""@example"",
+            ""meta_title"": null,
+            ""meta_description"": null,
+            ""url"": ""https://docs.ghost.io/author/cameron/""
+        }
+    ]
+}
+";
+
+    [SetUp]
+    public void Setup() { }
+
+    [Test]
+    public void TestJson()
+    {
+        var authorsQueryResult = JsonSerializer.Deserialize<AuthorsQueryResult>(
+            testJson,
+            new JsonSerializerOptions() { }
+        );
+        Assert.NotNull(authorsQueryResult);
+        TestContext.Progress.WriteLine(
+            $"{authorsQueryResult} {JsonSerializer.Serialize<AuthorsQueryResult>(authorsQueryResult)}"
+        );
+        Assert.NotNull(authorsQueryResult.Authors);
+        Assert.That(authorsQueryResult.Authors.Count, Is.EqualTo(1));
+
+        var author = authorsQueryResult.Authors[0];
+        Assert.NotNull(author);
+
+        Assert.That(author.Slug, Is.EqualTo("cameron"));
+        Assert.That(author.Id, Is.EqualTo("5ddc9b9510d8970038255d02"));
+        Assert.That(author.Name, Is.EqualTo("Cameron Almeida"));
+        Assert.That(
+            author.ProfileImage,
+            Is.EqualTo(
+                "https://docs.ghost.io/content/images/2019/03/1c2f492a-a5d0-4d2d-b350-cdcdebc7e413.jpg"
+            )
+        );
+        Assert.That(author.Bio, Is.EqualTo("Editor at large."));
+        Assert.That(author.Website, Is.EqualTo("https://example.com"));
+        Assert.That(author.Location, Is.EqualTo("Cape Town"));
+        Assert.That(author.Facebook, Is.EqualTo("example"));
+        Assert.That(author.Twitter, Is.EqualTo("@example"));
+        Assert.That(author.Url, Is.EqualTo("https://docs.ghost.io/author/cameron/"));
+    }
+}
