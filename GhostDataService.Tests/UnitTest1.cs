d50cd911cda0f74ee9dced50254f43b3e3e71f6a
Chris Helmich <kagekirin@gmail.com>
2022-05-31 11:50:27 +0900

    feature: add GhostDataService unit tests

---
+++ GhostDataService.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace GhostDataService.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
